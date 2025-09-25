415a05f56ea2f9a72b31c61ddffa95f07abfd07d
Nikotine1 <faq@faq.be>
2025-09-25 17:59:37 +0200

    Comment

--- src-core/common/graphite/graphite.h
+++ src-core/common/graphite/graphite.h
@@ -3,10 +3,9 @@
 #include <cstddef>   // for size_t
 #include <string>    // for string
 
-// Declarations (interface)
 int connect_graphite(void);
 void send_to_graphite(const std::string &message);
 
-// You might also want to expose setters for server/port
+// Setters for server/port
 void set_graphite_server(const char *server, int port);
 void set_graphite_debug(bool enable);
